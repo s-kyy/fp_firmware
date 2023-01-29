3435f34a05fe73b741601ac532898c5a1ad2aa33
s-kyy <samantha.y.twentyfourteen@gmail.com>
2023-01-29 16:01:06 -0500

    first working keymap + configs

--- keyboards/fingerpunch/ffkb_byomcu/v3/keymaps/s-kyy/keymap.c
+++ keyboards/fingerpunch/ffkb_byomcu/v3/keymaps/s-kyy/keymap.c
@@ -2,12 +2,12 @@
  * 
  */
 
-#include "sadekbaroudi.h"
+// #include "sadekbaroudi.h"
 #include QMK_KEYBOARD_H
 
 #ifdef ENCODER_ENABLE
-  bool is_alt_tab_active = false;
-  uint16_t alt_tab_timer = 0;
+bool is_alt_tab_active = false;
+uint16_t alt_tab_timer = 0;
 #endif
 
 // Defines names for use in layer keycodes and the keymap
@@ -17,12 +17,16 @@ enum layer_names {
     _LOWER,
     _MOUSE_NUM,
     _GAME,
-    _QWERT
+    _QWERTY
 };
 
-//name macros
+//custom keycodes
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
+#define COLDH TO(_COLEMAK)
+#define MOUSE TO(_MOUSE_NUM)
+#define GAME  TO(_GAME)
+#define QWERTY TO(_QWERTY)
 #define REDO LCTL(KC_Y)
 #define SFTT LSFT_T(KC_TAB) //Tab-tap Shift-hold
 #define SALT LALT_T(KC_SPC) //Space-tap LALT-hold
@@ -48,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,  KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,     KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_DEL,  
   _______,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,          KC_TILD,   KC_LBRC,  KC_RBRC,  KC_MINS,  KC_EQL,   KC_F11,
   _______,  KC_6,   KC_7,    KC_8,    KC_9,    KC_0,          KC_GRV,    KC_LPRN,  KC_RPRN,  KC_PSLS,  KC_BSLS,  KC_F12,
-                    _______, _______, _______, _______,       KC_BTN2,   _______,  _______,  _______
+                    _______, _______, _______, _______,       KC_BTN2,   _______,  MOUSE,    _______
 ),  
 
 // ============================================================
@@ -59,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_MYCM, _______,  REDO,    _______, _______,      _______,            KC_HOME,       KC_UP,    KC_END,       _______, KC_DEL,
   _______, KC_CAPS, KC_CUT,   KC_COPY, KC_PSTE, _______,      FP_SUPER_CTRL_TAB,  KC_LEFT,       KC_DOWN,  KC_RGHT,      _______, _______,
   _______, _______, _______,  KC_UNDO, _______, _______,      _______,            LCTL(KC_BSPC), _______,  LCTL(KC_DEL), _______, _______,
-                    KC_RALT,  _______,   _______, _______,      _______,            _______,       _______,  _______
+                    KC_RALT,  MOUSE,   _______, _______,      _______,            _______,       _______,  _______
 ),
 
 // ============================================================
@@ -69,17 +73,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_MYCM, _______,       KC_HASH, _______, _______,         KC_NUM,   KC_P7,   KC_P8,   KC_P9, KC_PSLS, _______,
   _______, KC_CAPS, FP_SCROLL_TOG, KC_BTN1, KC_BTN2, _______,         KC_CALC,  KC_P4,   KC_P5,   KC_P6, KC_PPLS, KC_PAST,
   _______, _______, _______,       _______, _______, _______,         KC_P0,    KC_P1,   KC_P2,   KC_P3, KC_PEQL, KC_PMNS,
-                    QK_RBT,        TO(4),   _______, _______,         KC_PDOT,  _______, TO(0),   TO(0)
+                    QK_RBT,        GAME,    _______, _______,         KC_PDOT,  _______, COLDH,   COLDH
 ),
 
 // ============================================================
 // Layer 4 - Game
 // ============================================================
 [_GAME] = LAYOUT_ffkb(
-  _______, KC_MYCM, _______,       KC_HASH, _______, _______,         KC_NUM,   KC_P7,   KC_P8,   KC_P9, KC_PSLS, _______,
-  _______, KC_CAPS, FP_SCROLL_TOG, KC_BTN1, KC_BTN2, _______,         KC_CALC,  KC_P4,   KC_P5,   KC_P6, KC_PPLS, KC_PAST,
-  _______, _______, _______,       _______, _______, _______,         KC_P0,    KC_P1,   KC_P2,   KC_P3, KC_PEQL, KC_PMNS,
-                    _______,       TO(5),   KC_SPC, LOWER,         _______,  LALT_T(KC_ENT), TO(0),   TO(0)
+  _______, KC_MYCM, _______,       KC_HASH, _______, _______,         KC_NUM,   KC_P7,          KC_P8,   KC_P9, KC_PSLS, _______,
+  _______, KC_CAPS, FP_SCROLL_TOG, KC_BTN1, KC_BTN2, _______,         KC_CALC,  KC_P4,          KC_P5,   KC_P6, KC_PPLS, KC_PAST,
+  _______, _______, _______,       _______, _______, _______,         KC_P0,    KC_P1,          KC_P2,   KC_P3, KC_PEQL, KC_PMNS,
+                    _______,       QWERTY,  KC_SPC,  LOWER,           _______,  LALT_T(KC_ENT), COLDH,   COLDH
 ),
 
 // ============================================================
@@ -89,13 +93,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,  KC_Q,   KC_W,     KC_E,   KC_R,     KC_T,         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     _______,
   _______,  KC_A,   KC_S,     KC_D,   KC_F,     KC_G,         KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  _______,
   _______,  KC_Z,   KC_X,     KC_C,   KC_V,     KC_B,         KC_N,     KC_M,     _______,  _______,  _______,  _______,
-                    _______,  RAISE,  _______,  _______,      _______,  _______,  LOWER,    TO(0)
+                    _______,  RAISE,  _______,  _______,      _______,  _______,  LOWER,    COLDH
 )
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _RAISE, _LOWER, _MOUSE_NUM);
-}
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//   return update_tri_layer_state(state, _RAISE, _LOWER, _MOUSE_NUM);
+// }
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {

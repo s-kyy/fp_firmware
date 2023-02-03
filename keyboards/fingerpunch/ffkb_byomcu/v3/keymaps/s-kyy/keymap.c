d60cdcb2b244b539eb41d7122d6f02d571fb8868
s-kyy <samantha.y.twentyfourteen@gmail.com>
2023-02-02 23:16:18 -0500

    fixed scroll layer --> scroll and snipe toggle, complete rgb indicator for numpad

--- keyboards/fingerpunch/ffkb_byomcu/v3/keymaps/s-kyy/keymap.c
+++ keyboards/fingerpunch/ffkb_byomcu/v3/keymaps/s-kyy/keymap.c
@@ -1,8 +1,7 @@
-/* @author Samantha Yuen
+/* @author Samantha Yuen 
  * 
  */
 
-// #include "sadekbaroudi.h"
 #include QMK_KEYBOARD_H
 
 #ifdef ENCODER_ENABLE
@@ -52,36 +51,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Layer 0 - Colemak-DH
 // ============================================================
 [_COLEMAK] = LAYOUT_ffkb(
-  KC_ESC,         KC_Q,     KC_W,     KC_F,   KC_P,   KC_B,       KC_J,   KC_L,   KC_U,     KC_Y,     KC_SCLN,    KC_BSPC,
-  OSM(MOD_LCTL),  KC_A,     KC_R,     KC_S,   KC_T,   KC_G,       KC_M,   KC_N,   KC_E,     KC_I,     KC_O,       KC_QUOT,
-  KC_LWIN,        KC_Z,     KC_X,     KC_C,   KC_D,   KC_V,       KC_K,   KC_H,   KC_COMM,  KC_DOT,   KC_SLSH,    KC_APP,
-                            KC_MUTE,  KC_BTN1,  SFTE, MRAISE,      LOWER,  SPSFT, MT(MOD_LALT,KC_TAB),    KC_RALT
+  KC_ESC,         KC_Q,     KC_W,     KC_F,   KC_P,   KC_B,     KC_J,   KC_L,   KC_U,     KC_Y,     KC_SCLN,    KC_BSPC,
+  OSM(MOD_LCTL),  KC_A,     KC_R,     KC_S,   KC_T,   KC_G,     KC_M,   KC_N,   KC_E,     KC_I,     KC_O,       KC_QUOT,
+  KC_LWIN,        KC_Z,     KC_X,     KC_C,   KC_D,   KC_V,     KC_K,   KC_H,   KC_COMM,  KC_DOT,   KC_SLSH,    KC_APP,
+                            KC_MUTE,  KC_BTN1,  SFTE, MRAISE,   LOWER,  SPSFT, MT(MOD_LALT,KC_TAB),    KC_RALT
 ),
 
 // ============================================================
 // Layer 1 - QWERTY
 // ============================================================
 [_QWERTY] = LAYOUT_ffkb(
-  _______, KC_Q,   KC_W,     KC_E,   KC_R,     KC_T,         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     _______,
-  _______, KC_A,   KC_S,     KC_D,   KC_F,     KC_G,         KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  _______,
-  _______, KC_Z,   KC_X,     KC_C,   KC_V,     KC_B,         KC_N,     KC_M,     _______,  _______,  _______,  _______,
-                   _______,  _______,_______,  _______,      _______,  _______,  _______,  _______
+  _______, KC_Q,   KC_W,     KC_E,   KC_R,     KC_T,        KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     _______,
+  _______, KC_A,   KC_S,     KC_D,   KC_F,     KC_G,        KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  _______,
+  _______, KC_Z,   KC_X,     KC_C,   KC_V,     KC_B,        KC_N,     KC_M,     _______,  _______,  _______,  _______,
+                   _______,  _______,_______,  _______,     _______,  _______,  _______,  _______
 ),
 
 // ============================================================
 // Layer 2 - Game
 // ============================================================
 [_GAME] = LAYOUT_ffkb(
-  _______, _______, KC_1, KC_2, KC_3, KC_4,                 KC_5,  KC_6,    KC_7,     KC_8,    KC_9,     _______,
-  KC_TAB,  KC_BTN1, KC_Q, KC_W, KC_E, KC_R,                 KC_Y,  KC_M,    KC_WH_U,  _______, _______,  COLDH,
-  KC_LSFT, KC_V,    KC_A, KC_S, KC_D, KC_F,                 KC_C,  KC_WH_L, KC_WH_D,  KC_WH_R, _______,  QWERTY,
-           _______, _______, KC_SPC,  _______,              _______, LALT_T(KC_ENT),  KC_LWIN,  EXITPARSEC 
+  _______, _______, KC_1, KC_2, KC_3, KC_4,          KC_5,  KC_6,    KC_7,     KC_8,    KC_9,     _______,
+  KC_TAB,  KC_BTN1, KC_Q, KC_W, KC_E, KC_R,          KC_Y,  KC_M,    KC_WH_U,  _______, _______,  COLDH,
+  KC_LSFT, KC_V,    KC_A, KC_S, KC_D, KC_F,          KC_C,  KC_WH_L, KC_WH_D,  KC_WH_R, _______,  QWERTY,
+           _______, _______, KC_SPC,  _______,       _______, LALT_T(KC_ENT),  KC_LWIN,  EXITPARSEC 
 ),
 
 // ============================================================
 // Layer 3 - RAISE
 // ============================================================
-// Pointing Device SNIPING LAYER
 [_RAISE] = LAYOUT_ffkb(
   KC_SLEP, KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,     KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_DEL,  
   KC_F11,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,          KC_TILD,   KC_LBRC,  KC_RBRC,  KC_MINS,  KC_PLUS,  KC_UNDS,
@@ -93,12 +91,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ============================================================
 // Layer 4 - LOWER
 // ============================================================
-// Pointing Device Scroll Layer
 [_LOWER] = LAYOUT_ffkb(
-  KC_ESC,        KC_MYCM,       TASKMAN,     UNDO,    WPAST,     _______,         C(KC_MINS),  KC_HOME,       KC_UP,              KC_END,      KC_BRIU,  KC_DEL,
-  OSM(MOD_LCTL), OSM(MOD_LSFT), CUT,         COPY,    PASTE,     _______,          C(KC_PLUS),  KC_LEFT,       KC_DOWN,            KC_RGHT,     KC_LALT,  KC_PGUP,
-  KC_LWIN,       KC_CAPS,      LDESK,       REDO,    RDESK,      _______,            _______,     C(KC_LEFT),    FP_SUPER_CTRL_TAB,  C(KC_RIGHT),  KC_BRID,  KC_PGDN,
-                                KC_RALT,     KC_BTN1, SFTE,      RGB_NUM,              _______,     _______,       _______,     _______
+  KC_ESC,        KC_MYCM,       TASKMAN,     UNDO,    WPAST,     _______,           _______,           KC_HOME,       KC_UP,              KC_END,      KC_BRIU,  KC_DEL,
+  OSM(MOD_LCTL), OSM(MOD_LSFT), CUT,         COPY,    PASTE,     C(KC_MINS),        FP_SCROLL_MOMENT,  KC_LEFT,       KC_DOWN,            KC_RGHT,     KC_LALT,  KC_PGUP,
+  KC_LWIN,       KC_CAPS,      LDESK,       REDO,     RDESK,     C(KC_PLUS),        FP_SNIPE_MOMENT,   C(KC_LEFT),    FP_SUPER_CTRL_TAB,  C(KC_RIGHT),  KC_BRID,  KC_PGDN,
+                                KC_RALT,     KC_BTN1, SFTE,      RGB_NUM,           _______,           _______,       _______,     _______
 ),
 
 // ============================================================
@@ -114,7 +111,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ============================================================
 // Empty Layers
 // ============================================================
-// Pointing Device Zooming Layer 
 [_LAYER6] = LAYOUT_ffkb(
   _______, _______, _______, _______, _______, _______,         _______,  _______,  _______,  _______, _______, _______,
   _______, _______, _______, _______, _______, _______,         _______,  _______,  _______,  _______, _______, _______,
@@ -128,6 +124,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,         _______,  _______,  _______,  _______, _______, _______,
                     _______, _______, _______, _______,         _______,  _______,  _______,  _______
 ),
+// Pointing Device SNIPING LAYER
+// Pointing Device Zooming Layer 
+// Pointing Device Scroll Layer
 [_LAYER8] = LAYOUT_ffkb(
   _______, _______, _______, _______, _______, _______,         _______,  _______,  _______,  _______, _______, _______,
   _______, _______, _______, _______, _______, _______,         _______,  _______,  _______,  _______, _______, _______,
@@ -180,25 +179,28 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 }
 #endif
         
+// ============================================================
 // Change RGB based on CAPSLOCK & NUMLOCK status
 // https://github.com/qmk/qmk_firmware/blob/master/docs/feature_rgb_matrix.md#common-configuration-idcommon-configuration
 // Generate Perfect Per-key RGB effects: see v3.c
 // QMK RGB Tutorial: https://github.com/sadekbaroudi/keyboard-guides/blob/master/firmware/qmk/RGB_MATRIX.md
 // RGB CODES: https://github.com/sadekbaroudi/qmk_firmware/tree/master/keyboards/fingerpunch#layer-lighting
+// ============================================================
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     if (host_keyboard_led_state().caps_lock) {
-        RGB_MATRIX_INDICATOR_SET_COLOR(5, 242, 191, 180); // CAPSLOCK ON | MCORRICK PAINTS CORAL #f2bfb4 https://encycolorpedia.com/f2bfb4
+        RGB_MATRIX_INDICATOR_SET_COLOR(5, 242, 242, 0); // CAPSLOCK ON | MCORRICK PAINTS CORAL #f2bfb4 https://encycolorpedia.com/f2bfb4
     } else {
         RGB_MATRIX_INDICATOR_SET_COLOR(5, 0, 0, 0); // CAPSLOCK OFF | BLANK
     }
     if (host_keyboard_led_state().num_lock) {
-        RGB_MATRIX_INDICATOR_SET_COLOR(26, 242, 191, 180); // NUMLOCK ON | Reaper Master New Gold (9051) #e7bd42 https://encycolorpedia.com/e7bd42
-        RGB_MATRIX_INDICATOR_SET_COLOR(25, 242, 191, 180); // CALC | MCORRICK PAINTS CORAL #f2bfb4 https://encycolorpedia.com/f2bfb4
-        RGB_MATRIX_INDICATOR_SET_COLOR(23, 242, 191, 180); // DOT | MCORRICK PAINTS CORAL #f2bfb4 https://encycolorpedia.com/f2bfb4
-        RGB_MATRIX_INDICATOR_SET_COLOR(37, 242, 191, 180); // PLUS | MCORRICK PAINTS CORAL #f2bfb4 https://encycolorpedia.com/f2bfb4
-        RGB_MATRIX_INDICATOR_SET_COLOR(5, 242, 191, 180); // EQUALS | MCORRICK PAINTS CORAL #f2bfb4 https://encycolorpedia.com/f2bfb4
-        RGB_MATRIX_INDICATOR_SET_COLOR(5, 242, 191, 180); // TIMES | MCORRICK PAINTS CORAL #f2bfb4 https://encycolorpedia.com/f2bfb4
-        RGB_MATRIX_INDICATOR_SET_COLOR(5, 242, 191, 180); // MINUS | MCORRICK PAINTS CORAL #f2bfb4 https://encycolorpedia.com/f2bfb4
+        // RGB_MATRIX_INDICATOR_SET_COLOR(26, 242, 191, 180); // NUMLOCK ON | Reaper Master New Gold (9051) #e7bd42 https://encycolorpedia.com/e7bd42
+        RGB_MATRIX_INDICATOR_SET_COLOR(26, 242, 242, 0); // NUMLOCK ON | yellow
+        RGB_MATRIX_INDICATOR_SET_COLOR(25, 242, 242, 0); // CALC | yellow
+        RGB_MATRIX_INDICATOR_SET_COLOR(23, 242, 242, 0); // DOT | yellow
+        RGB_MATRIX_INDICATOR_SET_COLOR(37, 242, 242, 0); // PLUS | yellow
+        RGB_MATRIX_INDICATOR_SET_COLOR(36, 242, 242, 0); // EQUALS | yellow
+        RGB_MATRIX_INDICATOR_SET_COLOR(40, 242, 242, 0); // TIMES | yellow
+        RGB_MATRIX_INDICATOR_SET_COLOR(38, 242, 242, 0); // MINUS | yellow
         
         RGB_MATRIX_INDICATOR_SET_COLOR(24, 252, 252, 252); // 0 | WHITE
         RGB_MATRIX_INDICATOR_SET_COLOR(29, 252, 252, 252); // 1 | WHITE
@@ -215,9 +217,9 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         RGB_MATRIX_INDICATOR_SET_COLOR(25, 0, 0, 0); // CALC 
         RGB_MATRIX_INDICATOR_SET_COLOR(23, 0, 0, 0); // DOT 
         RGB_MATRIX_INDICATOR_SET_COLOR(37, 0, 0, 0); // PLUS 
-        RGB_MATRIX_INDICATOR_SET_COLOR(5, 0, 0, 0); // EQUALS 
-        RGB_MATRIX_INDICATOR_SET_COLOR(5, 0, 0, 0); // TIMES 
-        RGB_MATRIX_INDICATOR_SET_COLOR(5, 0, 0, 0); // MINUS 
+        RGB_MATRIX_INDICATOR_SET_COLOR(36, 0, 0, 0); // EQUALS 
+        RGB_MATRIX_INDICATOR_SET_COLOR(40, 0, 0, 0); // TIMES 
+        RGB_MATRIX_INDICATOR_SET_COLOR(38, 0, 0, 0); // MINUS 
         
         RGB_MATRIX_INDICATOR_SET_COLOR(29, 0, 0, 0); // 1 | WHITE
         RGB_MATRIX_INDICATOR_SET_COLOR(24, 0, 0, 0); // 0 | WHITE

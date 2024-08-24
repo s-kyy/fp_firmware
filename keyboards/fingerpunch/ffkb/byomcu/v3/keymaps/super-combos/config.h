2363be9a6d29c8cdf682da70e97ff64bb692fda2
s-kyy <samantha.y.twentyfourteen@gmail.com>
2024-08-24 00:35:36 -0400

    created super-combos layout for ffkbv3

---
+++ keyboards/fingerpunch/ffkb/byomcu/v3/keymaps/super-combos/config.h
@@ -0,0 +1,121 @@
+/* Copyright 2024 Samantha Yuen <samantha.y.twentyfourteen@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published bynnnn
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+RESOURCES:
+Fingerpunch Layer CONFIG: https://github.com/sadekbaroudi/qmk_firmware/tree/master/keyboards/fingerpunch
+RGB MATRRIX Effect Summary: https://github.com/qmk/qmk_firmware/blob/master/docs/feature_rgb_matrix.md#rgb-matrix-effects-idrgb-matrix-effects
+RGB MATRIX Demo: https://youtu.be/7f3usatOIKM?si=9c02Iis5nooXlHvv
+*/
+
+// =============================================
+// Key config
+// =============================================
+#define FP_SUPER_TAB_TIMEOUT 500 // 500 default 
+#define FP_SUPER_CTRL_TAB_TIMEOUT 500 // 750 default
+// #define ENCODER_RESOLUTION 4 //input commands more than once.
+#define ONESHOT_TAP_TOGGLE 3
+#define ONESHOT_TIMEOUT 3000
+#define TAPPING_TOGGLE 3 // into mouse layer
+
+// Home Row Mods
+// #define TAPPING_TERM 230
+// #define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
+// #define PERMISSIVE_HOLD_PER_KEY
+#define USB_SUSPEND_WAKEUP_DELAY 3000
+// #define COMBO_COUNT 1 // DONE automatically
+#define COMBO_TERM 40 //40 default
+#define COMBO_MUST_HOLD_MODS
+#define COMBO_HOLD_TERM 140 //150 default
+#define COMBO_ONLY_FROM_LAYER 0
+
+// =============================================
+// Trackball config
+// =============================================
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+#define AUTO_MOUSE_DEFAULT_LAYER 6
+#define FP_AUTO_MOUSE_TRACKBALL_SENSITIVITY 3       // 3 default, Higher = mouse needs to move more to trigger mouse layers
+// #define MOUSE_EXTENDED_REPORT                    // Mouse trackball acceleration
+// #define FP_POINTING_EXTENDED_MOUSE_REPORT_DISABLE
+// #define FP_POINTING_DEFAULT_DPI 750  //1000 set to general v3 config file in increments of 100
+#define FP_POINTING_SCROLLING_DPI 1     // Dareu LM108 = 1000/1600
+// #define FP_POINTING_LAYER_ENABLE 
+// #define FP_POINTING_ZOOM_LAYER 2 
+#define FP_POINTING_SCROLLING_LAYER_ENABLE
+#define FP_POINTING_SCROLLING_LAYER 2 //INACCESSIBLE
+// #define FP_POINTING_SNIPING_LAYER_ENABLE
+// #define FP_POINTING_SNIPING_LAYER 8 //INACCESSIBLE
+// #define FP_POINTING_SNIPING_DPI 50 
+
+// =============================================
+// RGB config
+// =============================================
+#ifdef RGB_MATRIX_ENABLE //udate vial.json if using it. 
+    #define FP_STARTUP_ANIMATION_DISABLE
+    #define FP_LAYER_LIGHT_MODE RGB_MATRIX_ALPHAS_MODS
+    #define RGB_DISABLE_WHEN_USB_SUSPENDED
+    #define RGB_MATRIX_DEFAULT_SPD 90
+    #define RGB_MATRIX_DEFAULT_VAL 40 // Sets the default brightness value, if none has been set
+    #define FP_LAYER_LIGHTING_AUTO_MOUSE_HUE HSV_BLACK
+    #define FP_LAYER_LIGHTING_AUTO_MOUSE_MODE RGB_MATRIX_PIXEL_RAIN
+
+    // #define DISABLE_RGB_MATRIX_BREATHING
+    #define DISABLE_RGB_MATRIX_BAND_SA
+    #define DISABLE_RGB_MATRIX_BAND_VAL
+    #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+    #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+    #define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+    #define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+    #define DISABLE_RGB_MATRIX_CYCLE_ALL
+    #define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+    #define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+    #define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+    #define DISABLE_RGB_MATRIX_DUAL_BEACON
+    #define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
+    #define DISABLE_RGB_MATRIX_CYCLE_SPIRAL
+    #define DISABLE_RGB_MATRIX_RAINBOW_BEACON
+    #define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+    // #define DISABLE_RGB_MATRIX_RAINDROPS
+    // #define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+    // #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+    // #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+    #define DISABLE_RGB_MATRIX_SPLASH
+    // #define DISABLE_RGB_MATRIX_MULTISPLASH
+    #define DISABLE_RGB_MATRIX_SOLID_SPLASH
+    #define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
+
+    #define FP_LAYER_LIGHTING_HUE_0 HSV_ORANGE
+    // #define FP_LAYER_LIGHTING_HUE_1 HSV_WHITE    
+    #define FP_LAYER_LIGHTING_HUE_2 FP_HSV_MELON 
+    #define FP_LAYER_LIGHTING_HUE_3 HSV_GOLDENROD
+    #define FP_LAYER_LIGHTING_HUE_4 FP_HSV_LAVENDER
+    #define FP_LAYER_LIGHTING_HUE_5 HSV_CORAL
+    // #define FP_LAYER_LIGHTING_HUE_6 HSV_LEMON
+    
+    #define FP_LAYER_LIGHTING_MODE_0 RGB_MATRIX_MULTISPLASH
+    #define FP_LAYER_LIGHTING_MODE_1 RGB_MATRIX_JELLYBEAN_RAINDROPS
+    #define FP_LAYER_LIGHTING_MODE_2 RGB_MATRIX_BREATHING
+    #define FP_LAYER_LIGHTING_MODE_3 RGB_MATRIX_BREATHING
+    #define FP_LAYER_LIGHTING_MODE_4 RGB_MATRIX_BREATHING
+    // #define FP_LAYER_LIGHTING_MODE_5 RGB_MATRIX_BREATHING
+    #define FP_LAYER_LIGHTING_MODE_6 RGB_MATRIX_SOLID_MULTISPLASH
+    #define FP_LAYER_LIGHTING_CAPS_LOCK_HUE FP_HSV_PEACH_ORANGE
+    // #define FP_LAYER_LIGHTING_CAPS_LOCK_MODE RGB_MATRIX_NONE
+#endif
---
+++ keyboards/fingerpunch/ffkb/byomcu/v3/keymaps/super-combos/keymap.c
@@ -0,0 +1,326 @@
+/* Copyright 2024 Samantha Yuen <samantha.y.twentyfourteen@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published bynnnn
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include QMK_KEYBOARD_H
+
+#ifdef ENCODER_ENABLE
+bool is_alt_tab_active = false;
+uint16_t alt_tab_timer = 0;
+#endif
+
+// ============================================================
+// Defines names for use in layer keycodes and the keymap
+// ============================================================
+enum layer_names {
+    _COLEMAK, //0
+    _QWERTY,  //1
+    _SYM,     //2
+    _MCR,     //3
+    _NAV,     //4
+    // _RGB,     //5
+    // _MOUSE,  //6
+    _NEW_SAFE_LAYER  //7
+};
+
+// ============================================================
+// Basic custom keycodes
+// ============================================================
+#define CUT LCTL(KC_X)
+#define COPY LCTL(KC_C)
+#define PASTE LCTL(KC_V)
+#define WPAST G(KC_V)
+#define UNDO C(KC_Z)
+#define REDO C(KC_Y)
+#define LDESK G(C(KC_LEFT)) 
+#define RDESK G(C(KC_RIGHT))
+#define EXITPARSEC C(A(S(KC_TILD)))
+#define TASKMAN C(A(KC_DEL))
+#define MBEE C(S(A(KC_BSLS)))
+#define MBEEL C(S(KC_LBRC))
+#define MBEER C(S(KC_RBRC))
+#define MBEEVU C(S(A(KC_UP)))
+#define MBEEVD C(S(A(KC_DOWN)))
+
+enum custom_keycodes {
+  QMK_BEST = SAFE_RANGE,
+  GITCOMM, 
+  GITPUSH,
+  R_ASSIGN,
+  NEW_SAFE_RANGE
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case GITCOMM:
+        if (record->event.pressed) {
+          // when keycode is pressed
+            SEND_STRING("git commit -m ''");
+        } else {
+          // when keycode is released
+        }
+        break;
+
+    case GITPUSH:
+        if (record->event.pressed) {
+            SEND_STRING("git push -u origin ");
+        } 
+        break;
+    
+    case R_ASSIGN:
+        if (record->event.pressed) {
+            SEND_STRING("<- ");
+        } 
+        break;
+    }
+    return true;
+};
+
+// ============================================================
+// Combos
+// ============================================================
+enum combos {
+  // LEFT - COLDH
+  ZX_WIN,
+  XC_ALT,
+  CD_CTRL,
+  TG_TICK,
+  QW_ESC,
+  RT_TAB,
+  // RIGHT - COLDH
+  MN_TILDE,
+  LU_RPRN,
+  NE_LPRN,
+  UY_RBRC,
+  EI_LBRC,
+  YS_RCBR,
+  IO_LCBR,
+  HD_BSLS,
+  DC_SLSH,
+  // CENTER - COLDH
+  GM_CAPS,
+
+  COMBO_LENGTH, 
+};
+
+uint16_t COMBO_LEN = COMBO_LENGTH; 
+
+// LEFT - COLDH
+const uint16_t PROGMEM zx_combo[] = {KC_Z, KC_X, COMBO_END};
+const uint16_t PROGMEM xc_combo[] = {KC_X, KC_C, COMBO_END};
+const uint16_t PROGMEM cd_combo[] = {KC_C, KC_D, COMBO_END};
+const uint16_t PROGMEM tg_combo[] = {KC_T, KC_G, COMBO_END};
+const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM rt_combo[] = {KC_R, KC_T, COMBO_END};
+// RIGHT - COLDH
+const uint16_t PROGMEM mn_combo[] = {KC_M, KC_N, COMBO_END};
+const uint16_t PROGMEM lu_combo[] = {KC_L, KC_U, COMBO_END};
+const uint16_t PROGMEM ne_combo[] = {KC_N, KC_E, COMBO_END};
+const uint16_t PROGMEM uy_combo[] = {KC_U, KC_Y, COMBO_END};
+const uint16_t PROGMEM ei_combo[] = {KC_E, KC_I, COMBO_END};
+const uint16_t PROGMEM ys_combo[] = {KC_Y, KC_SCLN, COMBO_END};
+const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM hd_combo[] = {KC_H, KC_COMM, COMBO_END};
+const uint16_t PROGMEM dc_combo[] = {KC_COMM, KC_DOT, COMBO_END};
+//CENTER - COLDH
+const uint16_t PROGMEM gm_combo[] = {KC_G, KC_M, COMBO_END};
+
+combo_t key_combos[] = {
+  // LEFT - COLDH
+  [ZX_WIN] = COMBO(zx_combo, OSM(MOD_LGUI)),
+  [XC_ALT] = COMBO(xc_combo, OSM(MOD_LALT)),
+  [CD_CTRL] = COMBO(cd_combo, OSM(MOD_LCTL)),
+  [TG_TICK] = COMBO(tg_combo, KC_GRV),
+  [QW_ESC] = COMBO(qw_combo, KC_ESC),
+  [RT_TAB] = COMBO(rt_combo, KC_TAB),
+  // RIGHT - COLDH
+  [MN_TILDE] = COMBO(mn_combo, KC_TILD),
+  [LU_RPRN] = COMBO(lu_combo, KC_RPRN),
+  [NE_LPRN] = COMBO(ne_combo, KC_LPRN),
+  [UY_RBRC] = COMBO(uy_combo, KC_RBRC),
+  [EI_LBRC] = COMBO(ei_combo, KC_LBRC),
+  [YS_RCBR] = COMBO(ys_combo, KC_RCBR),
+  [IO_LCBR] = COMBO(io_combo, KC_LCBR),
+  [HD_BSLS] = COMBO(hd_combo, KC_BSLS),
+  [DC_SLSH] = COMBO(dc_combo, KC_SLSH),
+  //CENTER - COLDH
+  [GM_CAPS] = COMBO(gm_combo, KC_CAPS)
+
+};
+
+// ============================================================
+// Keymap
+// ============================================================
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+// ============================================================
+// Layer 0 - Colemak-DH
+// ============================================================
+[_COLEMAK] = LAYOUT(
+  KC_MS_BTN1,     KC_Q,     KC_W,        KC_F,     KC_P,    KC_B,                         KC_J,   KC_L,    KC_U,     KC_Y,     KC_DEL,       KC_BSPC,
+  OSM(MOD_LCTL),  KC_A,     KC_R,        KC_S,     KC_T,    KC_G,                         KC_M,   KC_N,    KC_E,     KC_I,     KC_O,         KC_SCLN,
+  KC_MS_BTN2,     KC_Z,     KC_X,        KC_C,     KC_D,    KC_V,                         KC_K,   KC_H,    KC_COMM,  KC_DOT,   KC_QUOT,      KC_APP,
+                  KC_MUTE,  KC_LALT,  LT(_SYM, KC_ENT),   LT(_MCR,FP_SUPER_CTRL_TAB),     OSM(MOD_LSFT), LT(_NAV,KC_SPC), KC_BSPC, KC_RALT
+),
+// ============================================================
+// Layer 1 - QWERTY
+// ============================================================
+[_QWERTY] = LAYOUT(
+  KC_TAB,        KC_Q,   KC_W,   KC_E,     KC_R,     KC_T,    KC_Y,                KC_U,     KC_I,     KC_O,     KC_P,       _______,
+  OSM(MOD_LCTL), KC_A,   KC_S,   KC_D,     KC_F,     KC_G,    KC_H,                KC_J,     KC_K,     KC_L,     KC_SCLN,    _______,
+  OSM(MOD_LSFT), KC_Z,   KC_X,   KC_C,     KC_V,     KC_B,    KC_N,                KC_M,     _______,  _______,  _______,    KC_BTN2,
+                _______,  _______,  LT(_SYM,KC_SPC),   LT(_MCR, KC_MS_BTN1),    KC_SPC,  _______,  KC_ENT,  _______
+),
+// ============================================================
+// Layer 2 - SYM
+// ============================================================
+[_SYM] = LAYOUT(
+  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_NUM,   KC_7,     KC_8,     KC_9, _______, _______,
+  LDESK,   KC_PIPE, KC_EQL, KC_UNDS, KC_MINS, KC_PAST,          KC_DOT,   KC_4,     KC_5,     KC_6, KC_PLUS, RDESK,
+  _______, _______, _______, KC_AMPR, KC_CIRC, _______,         KC_0,     KC_1,     KC_2,     KC_3, KC_SLSH, _______,
+                    _______, _______, _______, _______,         _______,  _______,  FP_SUPER_TAB,  EE_CLR
+),
+// ============================================================
+// Layer 3 - Macros
+// ============================================================
+[_MCR] = LAYOUT(
+  KC_ESC,   TASKMAN,  C(S(KC_V)), UNDO,   WPAST,        TO(_QWERTY),      _______,    KC_BRID, KC_BRIU,   MBEEVD,  MBEEVU,  _______, 
+  LDESK,    C(KC_A),  CUT,        COPY,   PASTE,        _______,          C(KC_EQL),  MBEEL,   MEH(KC_P), MBEER,   MBEE,    RDESK,
+  _______,  _______,  _______,    REDO,   G(S(KC_S)),   TO(_COLEMAK),     C(KC_MINS), KC_PSCR, KC_SCRL,   KC_PAUS, _______, _______,
+                                 _______,  _______, _______, _______,     KC_BTN3,  _______,  _______,  EE_CLR
+),
+// ============================================================
+// Layer 4 - NAV_NEW
+// ============================================================
+[_NAV] = LAYOUT(
+  KC_ESC,      RGB_TOG, RGB_MOD, RGB_RMOD, RGB_HUD, RGB_HUI,                                KC_PGUP, KC_HOME,   KC_UP,    KC_END,   KC_DEL,   _______,
+  EXITPARSEC,  OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_MEH),    KC_PGDN, KC_LEFT,   KC_DOWN,  KC_RGHT,  KC_BSPC,  KC_INS,
+  KC_F12,      KC_F1,  KC_F2, KC_F3, KC_F4,  KC_F5,                                         KC_F6,   KC_F7,     KC_F8,    KC_F9,    KC_F10,   KC_F11,
+                     QK_BOOT, _______, _______, _______,                                    _______, _______,   _______,  _______
+),
+// ============================================================
+// Empty Layer
+// ============================================================
+[_NEW_SAFE_LAYER] = LAYOUT(
+  _______, _______, _______, _______, _______, _______,         _______,  _______,  _______,  _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,         _______,  _______,  _______,  _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,         _______,  _______,  _______,  _______, _______, _______,
+                    _______, _______, _______, _______,         _______,  _______,  _______,  _______
+)
+};
+
+// ============================================================
+// Encoder button settings
+// ============================================================
+#ifdef ENCODER_ENABLE
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    // default behavior if undefined
+    if (index == 0) {
+        // Conditional to reverse the direction of encoder number 1
+        // The reason I have this is that for some of my boards, it supports two different types of encoders, and they may differ in direction
+        #ifdef ENCODERS_A_REVERSE
+        if (!clockwise) {
+        #else
+        if (clockwise) {
+        #endif
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } 
+    else if (index == 1) {
+      // Conditional to reverse the direction of encoder number 1
+      // The reason I have this is that for some of my boards, it supports two different types of encoders, and they may differ in direction
+      #ifdef ENCODERS_B_REVERSE
+      if (!clockwise) {
+      #else
+      if (clockwise) {
+      #endif
+        // tap_code16(FP_SUPER_TAB);
+        // tap_code16(KC_TAB);
+        // tap_code16(KC_PGUP);
+        tap_code16(MBEEL);
+        // press_super_tab(false);
+      }
+      else{
+        // tap_code16(LSFT_T(FP_SUPER_TAB));
+        // tap_code16(LSFT_T(KC_TAB));
+        // tap_code16(KC_PGDN);
+        tap_code16(MBEER);
+        // press_super_tab(true);
+      }
+    }
+
+    return true;
+}
+#endif
+        
+// ============================================================
+// Change RGB based on CAPSLOCK & NUMLOCK status
+// https://github.com/qmk/qmk_firmware/blob/master/docs/feature_rgb_matrix.md#common-configuration-idcommon-configuration
+// Generate Perfect Per-key RGB effects: see v3.c
+// QMK RGB Tutorial: https://github.com/sadekbaroudi/keyboard-guides/blob/master/firmware/qmk/RGB_MATRIX.md
+// RGB CODES: https://github.com/sadekbaroudi/qmk_firmware/tree/master/keyboards/fingerpunch#layer-lighting
+// ============================================================
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (host_keyboard_led_state().caps_lock) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(6, 242, 242, 0); // CAPSLOCK ON | yellow / MCORRICK PAINTS CORAL #f2bfb4 https://encycolorpedia.com/f2bfb4
+    } else {
+        RGB_MATRIX_INDICATOR_SET_COLOR(6, 0, 0, 0); // CAPSLOCK OFF | BLANK
+    }
+    if (host_keyboard_led_state().num_lock) {
+        // RGB_MATRIX_INDICATOR_SET_COLOR(26, 242, 191, 180); // NUMLOCK ON | Reaper Master New Gold (9051) #e7bd42 https://encycolorpedia.com/e7bd42
+        RGB_MATRIX_INDICATOR_SET_COLOR(26, 242, 242, 0); // NUMLOCK ON | yellow
+        // RGB_MATRIX_INDICATOR_SET_COLOR(25, 242, 242, 0); // CALC | yellow
+        // RGB_MATRIX_INDICATOR_SET_COLOR(23, 242, 242, 0); // DOT | yellow
+        // RGB_MATRIX_INDICATOR_SET_COLOR(37, 242, 242, 0); // PLUS | yellow
+        // RGB_MATRIX_INDICATOR_SET_COLOR(36, 242, 242, 0); // EQUALS | yellow
+        // RGB_MATRIX_INDICATOR_SET_COLOR(40, 242, 242, 0); // TIMES | yellow
+        // RGB_MATRIX_INDICATOR_SET_COLOR(38, 242, 242, 0); // MINUS | yellow
+        
+        // RGB_MATRIX_INDICATOR_SET_COLOR(24, 252, 252, 252); // 0 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(29, 252, 252, 252); // 1 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(30, 252, 252, 252); // 2 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(35, 252, 252, 252); // 3 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(28, 252, 252, 252); // 4 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(31, 252, 252, 252); // 5 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(34, 252, 252, 252); // 6 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(27, 252, 252, 252); // 7 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(32, 252, 252, 252); // 8 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(33, 252, 252, 252); // 9 | WHITE
+    } else {
+        RGB_MATRIX_INDICATOR_SET_COLOR(26, 0, 0, 0); // NUMLOCK OFF | BLANK
+        // RGB_MATRIX_INDICATOR_SET_COLOR(25, 0, 0, 0); // CALC 
+        // RGB_MATRIX_INDICATOR_SET_COLOR(23, 0, 0, 0); // DOT 
+        // RGB_MATRIX_INDICATOR_SET_COLOR(37, 0, 0, 0); // PLUS 
+        // RGB_MATRIX_INDICATOR_SET_COLOR(36, 0, 0, 0); // EQUALS 
+        // RGB_MATRIX_INDICATOR_SET_COLOR(40, 0, 0, 0); // TIMES 
+        // RGB_MATRIX_INDICATOR_SET_COLOR(38, 0, 0, 0); // MINUS 
+        
+        // RGB_MATRIX_INDICATOR_SET_COLOR(29, 0, 0, 0); // 1 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(24, 0, 0, 0); // 0 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(30, 0, 0, 0); // 2 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(35, 0, 0, 0); // 3 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(28, 0, 0, 0); // 4 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(31, 0, 0, 0); // 5 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(34, 0, 0, 0); // 6 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(27, 0, 0, 0); // 7 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(32, 0, 0, 0); // 8 | WHITE
+        // RGB_MATRIX_INDICATOR_SET_COLOR(33, 0, 0, 0); // 9 | WHITE
+    }
+
+    return false;
+}
